1ccf7a2827aed6bc29cde3a7afd0b21103079462
Omar Aly Mahran <58120991+Mahran-99@users.noreply.github.com>
2022-06-13 22:22:32 +0200

    update currency

--- src/test/java/StepDefinitions/SD05_Currencies.java
+++ src/test/java/StepDefinitions/SD05_Currencies.java
@@ -3,6 +3,7 @@ package StepDefinitions;
 import Pages.P05_Currencies;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
@@ -11,6 +12,7 @@ import java.util.List;
 public class SD05_Currencies {
 
     P05_Currencies curr = new P05_Currencies();
+    JavascriptExecutor js = (JavascriptExecutor) Hooks.driver;
 
     @Given("user selects \"(.*)\" from drop down menu$")
     public void selectCurrency(String currency) {
@@ -18,9 +20,11 @@ public class SD05_Currencies {
     }
 
     @Then("currency is selected and displayed on the 4 products in Home page")
-    public void displayCurrency() {
+    public void displayCurrency() throws InterruptedException {
         List<WebElement> webElements = curr.displayCurrency();
         System.out.println("Number of Products on homepage: " + webElements.size());
+        js.executeScript("arguments[0].scrollIntoView();", webElements.get(0));
+        Thread.sleep(2000);
         for (WebElement webElement : webElements) {
             Assert.assertTrue(webElement.getText().contains("€"));
         }
